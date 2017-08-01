8a2b7c866218a610d1aa7be24c59e71a734e3d80
Caio Oliveira <caio.oliveira07@gmail.com>
2017-08-01 17:35:37 -0300

    Pagina Participantes funcionando

--- js/main.js
+++ js/main.js
@@ -21,3 +21,38 @@ $(document).ready(function(){
 		$(this).toggleClass('open');
 	});
 });
+
+
+function popupOpenClose(popup) {
+    
+    /* Add div inside popup for layout if one doesn't exist */
+    if ($(".wrapper-popup").length == 0){
+        $(popup).wrapInner("<div class='wrapper-popup'></div>");
+    }
+    
+    /* Open popup */
+    $(popup).show();
+
+    /* Close popup if user clicks on background */
+    $(popup).click(function(e) {
+        if ( e.target == this ) {
+            if ($(popup).is(':visible')) {
+                $(popup).hide();
+            }
+        }
+    });
+
+    /* Close popup and remove errors if user clicks on cancel or close buttons */
+    $(popup).find("button[name=close]").on("click", function() {
+        if ($(".formElementError").is(':visible')) {
+            $(".formElementError").remove();
+        }
+        $(popup).hide();
+    });
+}
+
+$(document).ready(function() {
+    $("[data-js=open]").on("click", function() {
+        popupOpenClose($(this).next(".popup"));
+    });
+});
